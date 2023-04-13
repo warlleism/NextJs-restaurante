7280627946c2300d6aa10669e4734ab672a97815
Warllei Martins <warllei.s.martins@hotmail.com>
2023-04-13 20:13:55 -0300

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from '../src/styles/Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar;
